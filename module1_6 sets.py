5849f846dbf7ee5e7a5c50b1d2c35a7ff6f0612d
Sat9995 <saidboy995@gmail.com>
2024-10-17 12:42:51 -0700

    Add files via upload

---
+++ module1_6 sets.py
@@ -0,0 +1,8 @@
+my_set = {1,1,1,2,2,2,4,5,6,4,4,3, 'string', 'true'}
+print(my_set)
+print(my_set.add('hope'))
+print(my_set.add('knowledge'))
+print(my_set)
+print(my_set.remove(2))
+print(my_set)
+
